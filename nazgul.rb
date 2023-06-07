c9e1e169d809ffbf92a9b3d52c753f6b5aaa9c19
Tony Yunker <tony.yunker@gmail.com>
2023-06-07 16:31:33 -0500

    print total_distance to index

--- nazgul.rb
+++ nazgul.rb
@@ -1,5 +1,8 @@
 require "sinatra"
+require "sequel"
 
 get "/" do
-  erb :index
+  conn = Sequel.connect("postgresql://localhost/nazgul")
+  total_distance = conn[:activities].where { activity_date > "2023-01-01" }.map(:distance).reduce(:+).to_f
+  erb :index, locals: {total_distance:}
 end
