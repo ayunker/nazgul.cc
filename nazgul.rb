835812b0a8e520dde48356621071e21a26ca6f71
Tony Yunker <tony.yunker@gmail.com>
2023-06-09 12:26:40 -0500

    wrap query in connection block to close conn afterwards

--- nazgul.rb
+++ nazgul.rb
@@ -2,7 +2,9 @@ require "sinatra"
 require "sequel"
 
 get "/" do
-  conn = Sequel.connect("postgresql://localhost/nazgul")
-  total_distance = conn[:activities].where { activity_date > "2023-01-01" }.map(:distance).reduce(:+).to_f
+  total_distance = 0
+  Sequel.connect("postgresql://localhost/nazgul") do |conn|
+    total_distance = conn[:activities].where { activity_date > "2023-01-01" }.map(:distance).reduce(:+).to_f
+  end
   erb :index, locals: {total_distance:}
 end
