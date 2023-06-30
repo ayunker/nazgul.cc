851a3b9ca9510c237444735b0be7b621c8a9dbb6
Tony Yunker <tony.yunker@gmail.com>
2023-06-30 13:24:38 -0500

    filter by activities with kat

--- nazgul.rb
+++ nazgul.rb
@@ -7,7 +7,13 @@ Dotenv.load
 get "/" do
   total_distance = 0
   Sequel.connect(ENV["DATABASE_URL"]) do |conn|
-    total_distance = conn[:activities].where { activity_date > "2023-01-01" }.map(:distance).reduce(:+).to_f
+    total_distance = conn[:activities]
+      .where { activity_date > "2023-01-01" }
+      .where(with_kat: true)
+      .map(:distance)
+      .reduce(:+)
+      .to_f
   end
+
   erb :index, locals: {total_distance:}
 end
