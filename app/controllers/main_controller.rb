5b5fd15f8fc5d103be02e64c012844bf896cb49c
Tony Yunker <tony.yunker@gmail.com>
2024-09-08 15:28:28 -0500

    feat: filter by with Kat

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
 class MainController < ApplicationController
   def index
-    @total_distance = Activity.by_year(2024).on_bike.sum(&:distance).to_f
+    @total_distance = Activity
+      .by_year(2024)
+      .on_bike
+      .with_kat
+      .sum(&:distance).to_f
   end
 end
